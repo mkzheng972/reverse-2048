c6b685cdac73af3c323e88732527e2fe551da153
mkzheng972 <markzheng27@gmail.com>
2020-12-21 01:41:00 -0500

    added sum to four movement functions to get the score for current game

--- client/app.js
+++ client/app.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Header from './components/header/header'
 import Routes from './routes'
-import Footer from './components/footer/footer'
 
 const App = () => {
   return (
